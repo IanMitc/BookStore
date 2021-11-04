315e805558d6e99e7155eccb52e2fd01968dc160
Ian Mitchell <ian.mitchell@revature.net>
2021-11-04 17:40:30 -0600

    Main Menu

---
+++ src/main/java/org/example/UI/Menus/Shared/Options/ExitOption.java
@@ -0,0 +1,7 @@
+package org.example.UI.Menus.Shared.Options;
+
+public class ExitOption {
+    public static void execute() {
+        System.out.println("I GUESS we can put back all these books from your shopping cart...");
+    }
+}
---
+++ src/main/java/org/example/UI/Menus/Shared/Options/LoginOption.java
@@ -0,0 +1,35 @@
+package org.example.UI.Menus.Shared.Options;
+
+import com.mitchell.ian.Permissions.Permissions;
+import org.example.Data.DaoFactory;
+import org.example.Data.UserDao;
+import org.example.Objects.User;
+import org.example.UI.Menus.Customer.CustomerMenu;
+import org.example.UI.Utility.Ask;
+import org.example.UI.Utility.Clear;
+
+import java.util.Currency;
+
+public class LoginOption {
+    public static void execute() {
+        Clear.console();
+
+        System.out.println("Please Login");
+        String username = Ask.forString("Username");
+        String password = Ask.forString("Password");
+
+        UserDao userDao = DaoFactory.getUserDao();
+        User user = userDao.getUser(username);
+
+        boolean loginSuccessful = user.checkPassword(password);
+
+        if (loginSuccessful) {
+            System.out.println("Logged In");
+            CustomerMenu.show();
+        } else {
+            System.out.println("Login Failed");
+            return;
+        }
+    }
+}
+
